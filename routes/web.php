267f821bc2eecdc4751b776ad877d84fec41afcd
carouse1123 <Theeraphong.s@kkumail.com>
2023-01-24 20:30:43 +0700

    admin login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')->middleware('auth')->group(function(){
+     Route::get('/dashboard',[AdminController::class, 'index'])->name('dashboard');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('welcome');
+
 });
